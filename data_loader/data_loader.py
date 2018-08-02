c3aead4db205069d9d181e9f0c588576f0b668e8
Taylor Jackle Spriggs <taylor@sigopt.com>
2018-08-02 11:05:25 -0700

    add segmentation feature

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -1,8 +1,13 @@
+class Segmentation(object):
+  def __init__(self, start, run):
+    self.start = start
+    self.run = run
+
 class Sample(object):
   def __init__(self, line):
     self.image, segmentation = line[:-1].split(',')
     segmentation = list(map(int, data.split(' ')))
-    self.segmentations = list(zip(segmentation[::2], segmentation[1::2]))
+    self.segmentations = [Segmentation(start_px, run) for start_px, run in zip(segmentation[::2], segmentation[1::2])]
 
 def load_segmentations(filename='train_ship_segmentations.csv'):
   with open(filename) as segmentations_fp:
