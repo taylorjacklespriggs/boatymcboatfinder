7ca1d37e4910ffbfe3656dcf16c2b5e8b984e9c0
taylorjacklespriggs <taylor@sigopt.com>
2018-08-02 20:53:38 +0000

    many fixes

--- data_loader/data_server.py
+++ data_loader/data_server.py
@@ -12,32 +12,40 @@ class SampleReader(object):
   closed = False
 
   def __init__(self, batch_size, image_size):
-    self.samples = iter(np.random.choice(samples, batch_size))
     self.image_size = image_size
-    self._load_next_image()
+    self.samples = (self._load_next_image(sample) for sample in np.random.choice(samples, batch_size))
+    self.current_buffer = next(self.samples)
 
-  def _load_next_image(self):
-    self.stream = io.BytesIO()
-    try:
-      sample = next(self.samples)
-    except StopIteration:
-      return
+  def _load_next_image(self, sample):
+    stream = io.BytesIO()
     image = sample.load_image()
-    self.stream.write(memoryview(image))
+    stream.write(memoryview(image))
     mask = np.zeros(image.shape[:2], dtype=np.uint8)
     sample.apply_segmentations(mask, 1)
-    self.stream.write(memoryview(mask))
-    self.stream.seek(0)
+    stream.write(memoryview(mask))
+    stream.seek(0)
+    return stream
 
-  def read(size=-1):
-    data = self.stream.read(size)
-    if size and not data:
-      self._load_next_image()
-    return data
+  def readinto(self, buff):
+    data = self.current_buffer.read(len(buff))
+    if not data:
+      try:
+        self.current_buffer = next(self.samples)
+        data = self.current_buffer.read(len(buff))
+      except StopIteration:
+        pass
+    count = len(data)
+    buff[:count] = data
+    return count
 
   def readable(self):
     return True
 
+  def close(self):
+    pass
+
+  def flush(self):
+    pass
 
 @app.route('/train_batch')
 def train_batch():
