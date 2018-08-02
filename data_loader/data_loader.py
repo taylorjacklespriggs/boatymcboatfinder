96066a7cf5a211732da54896e1bb19c751a085ad
Taylor Jackle Spriggs <taylor@sigopt.com>
2018-08-02 11:10:54 -0700

    samples and segmentations

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -3,12 +3,23 @@ class Segmentation(object):
     self.start = start
     self.run = run
 
+  def __repr__(self):
+    return 'Segmentation(start={}, run={})'.format(self.start, self.run)
+
 class Sample(object):
   def __init__(self, line):
     self.image, segmentation = line[:-1].split(',')
-    segmentation = list(map(int, data.split(' ')))
+    segmentation = list(map(int, segmentation.split(' ')))
     self.segmentations = [Segmentation(start_px, run) for start_px, run in zip(segmentation[::2], segmentation[1::2])]
 
-def load_segmentations(filename='train_ship_segmentations.csv'):
+  def __repr__(self):
+    return 'Sample(n_segmentations={})'.format(len(self.segmentations))
+
+def load_samples(filename='train_ship_segmentations.csv'):
   with open(filename) as segmentations_fp:
     return [Sample(line) for line in segmentations_fp]
+
+if __name__ == '__main__':
+  sample = next(iter(load_samples()))
+  print(sample)
+  print(next(iter(sample.segmentations)))
