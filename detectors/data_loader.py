ddb0dec7e6cccfa83a4439665d2ba77e6848d3fe
Taylor Jackle Spriggs <taylor@sigopt.com>
2018-08-02 23:38:13 -0700

    updates

--- detectors/data_loader.py
+++ detectors/data_loader.py
@@ -15,7 +15,7 @@ class TrainingDataLoader(object):
       self.image_size,
     )
 
-  def load_train_batch(self):
+  def load_batch(self):
     with urlopen(self.get_train_url()) as train_fp:
       x = np.frombuffer(train_fp.read(self.batch_pixels * 3), dtype=np.uint8)
       x = x.reshape((self.batch_size, self.image_size, self.image_size, 3)).astype(np.float32)
@@ -23,3 +23,16 @@ class TrainingDataLoader(object):
       y = np.frombuffer(train_fp.read(self.batch_pixels), dtype=np.uint8)
       y = y.reshape((self.batch_size, self.image_size, self.image_size, 1)).astype(np.float32)
       return x, y
+
+def load_evaluation_data(server):
+  url = 'http://{}/evaluation_batch'.format(self.server)
+  full_size = 768
+
+  with urlopen(url) as eval_fp:
+    count = np.frombuffer(eval_fp.read(4), dtype=np.uint32)[0]
+    x = np.frombuffer(eval_fp.read(count * 3), dtype=np.uint8)
+    x = x.reshape((count, full_size, full_size, 3)).astype(np.float32)
+    x /= 255
+    y = np.frombuffer(train_fp.read(count), dtype=np.uint8)
+    y = y.reshape((count, full_size, full_size, 1)).astype(np.float32)
+    return x, y
--- detectors/evaluator.py
+++ detectors/evaluator.py
@@ -8,32 +8,27 @@ from constants import assignments, x, y
 
 import galileo_io
 
-evaluations = 200
-
 if __name__ == '__main__':
+  server = 'ec2-54-245-11-96.us-west-2.compute.amazonaws.com:5000'
   training_loader = TrainingDataLoader(
-      'ec2-54-245-11-96.us-west-2.compute.amazonaws.com:5000',
+    server,
     batch_size=assignments['batch_size'],
     image_size=assignments['image_size'],
   )
-  evaluation_loader = TrainingDataLoader(
-      'ec2-54-245-11-96.us-west-2.compute.amazonaws.com:5000',
-    batch_size=1,
-    image_size=768,
-  )
+  x_eval, y_eval = load_evaluation_data(server)
   with tf.Session() as sess:
     model = ModelV1()
     sess.run(tf.global_variables_initializer())
     start_train = time.time()
     for _ in range(assignments['batches']):
-      batch = training_loader.load_train_batch()
+      batch = training_loader.load_batch()
       print(model.train_batch(sess, batch))
     galileo_io.log_metric('negative_train_time', start_train - time.time())
     start_eval = time.time()
     loss = sum(
-      model.evaluate(sess, evaluation_loader.load_train_batch())
-      for _ in range(evaluations)
-    ) / evaluations
+      model.evaluate(sess, (x_test.reshape((1, -1)), y_test.reshape((1, -1))))
+      for x_test, y_test in zip(x_eval, y_eval)
+    ) / len(x_eval)
     galileo_io.log_metric('negative_eval_time', start_eval - time.time())
     print('final loss', loss)
     galileo_io.log_metric('negative_log_loss', -math.log(loss))
