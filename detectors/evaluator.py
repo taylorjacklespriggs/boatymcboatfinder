8d032df6b8b91aded5b3d07eeec602fcd4fbe9d9
Taylor Jackle Spriggs <taylor@sigopt.com>
2018-08-08 16:05:54 -0700

    iout

--- detectors/evaluator.py
+++ detectors/evaluator.py
@@ -8,21 +8,15 @@ from constants import assignments, x, y
 def evaluate_model(session, model, evaluation_data):
   count = 10
   x_eval, y_eval = evaluation_data
-  blank_error = 0.
-  blank_total = 0.
-  boats_error = 0.
-  boats_total = 0.
+  total_intersection, total_union = 0., 0.
   for i in range(0, len(x_eval), count):
       end = min(i + count, len(x_eval))
       spliced = x_eval[i:end], y_eval[i:end]
-      blank_pred, blank, boat_pred, boat = model.evaluate(session, spliced)
-      blank_error += blank_pred
-      blank_total += blank
-      boats_error += boat_pred
-      boats_total += boat
-      print('blank error', blank_error, 'with weight', blank_total)
-      print('boats error', boats_error, 'with weight', boats_total)
-  return blank_error, blank_total, boats_error, boats_total
+      intersection, union = model.evaluate(session, spliced)
+      total_intersection += intersection
+      total_union += union
+      print('intersection', intersection, 'and union', union)
+  return intersection, union
 
 def train_and_evaluate(model_gen):
   import galileo_io
@@ -53,11 +47,12 @@ def train_and_evaluate(model_gen):
     galileo_io.log_metadata('average_load_time', load_time / batches)
     galileo_io.log_metadata('average_batch_time', batch_time / batches)
     start_eval = time.time()
-    blank_error, blank_total, boats_error, boats_total = evaluate_model(sess, model, evaluation_data)
-    blank_loss = blank_error / blank_total
-    boat_loss = boats_error / boats_total
+    intersection, union = evaluate_model(sess, model, evaluation_data)
     galileo_io.log_metric('negative_eval_time', start_eval - time.time())
-    print('final blank', blank_loss, 'boats', boat_loss)
-    galileo_io.log_metadata('blank_loss', blank_loss)
-    galileo_io.log_metadata('boat_loss', boat_loss)
-    galileo_io.log_metric('negative_log_loss', -math.log(blank_loss + boat_loss))
+    print('final intersection', intersection, 'union', union)
+    print('iou', intersection / union)
+    galileo_io.log_metadata('intersection', intersection)
+    galileo_io.log_metadata('union', union)
+    iou = intersection / union
+    galileo_io.log_metadata('iou', iou)
+    galileo_io.log_metric('loss', 1. - iou)
--- detectors/model_base.py
+++ detectors/model_base.py
@@ -6,23 +6,15 @@ from constants import assignments, optimizer, x, y, training_mode
 class ModelBase(object):
   def __init__(self):
     self.model = self.create_model()
-    squared_diff = tf.square(y - self.model)
-    blank_pixels = 1. - y
-    self.total_blank = tf.reduce_sum(blank_pixels)
-    self.error_blank = tf.reduce_sum(blank_pixels * squared_diff)
-    self.blank_loss = tf.cond(
-      self.total_blank > 0.,
-      true_fn=lambda: self.error_blank / self.total_blank,
-      false_fn=lambda: 0.,
+    self.masked_output = y * self.model
+    self.intersection = tf.reduce_sum(self.masked_output)
+    self.union = tf.reduce_sum(y + self.model - self.masked_output)
+    self.iou = self.intersection / self.union
+    self.loss = tf.cond(
+      self.union > 0.,
+      true_fn=lambda: 1. - self.iou,
+      false_fn=lambda: self.model,
     )
-    self.total_boat = tf.reduce_sum(y)
-    self.error_boat = tf.reduce_sum(y * squared_diff)
-    self.boat_loss = tf.cond(
-      self.total_boat > 0.,
-      true_fn=lambda: self.error_boat / self.total_boat,
-      false_fn=lambda: 0.,
-    )
-    self.loss = self.boat_loss + self.blank_loss
     self.optimizer = optimizer(
       learning_rate=10**assignments.get('log_learning_rate', -3)
     )
@@ -30,11 +22,10 @@ class ModelBase(object):
 
   def train_batch(self, session, batch):
     x_train, y_train = batch
-    _, loss = session.run(
+    return = session.run(
       [self.optimizer, self.loss],
       feed_dict={x: x_train, y: y_train, training_mode: True}
-    )
-    return loss
+    )[1]
 
   def forward(self, session, x_data):
     return session.run(
@@ -45,6 +36,6 @@ class ModelBase(object):
   def evaluate(self, session, batch):
     x_train, y_train = batch
     return session.run(
-      [self.error_blank, self.total_blank, self.error_boat, self.total_boat],
+      [self.intersection, self.union],
       feed_dict={x: x_train, y: y_train, training_mode: False}
     )
