ec002caf1a31849ef5ee1304fe238bc2900d0553
Taylor Jackle Spriggs <taylor@sigopt.com>
2018-08-15 18:28:49 -0700

    update movel v2

--- detectors/model_v2.py
+++ detectors/model_v2.py
@@ -5,29 +5,40 @@ from model_base import ModelBase
 
 def create_conv_block(in_tensor, kernel, output, activation):
     in_features = in_tensor.get_shape()[3].value
-    w_c = tf.Variable(tf.random_normal([kernel, kernel, in_features, output]))
-    b_c = tf.Variable(tf.random_normal([output]))
+    total_in_size = kernel**2 * in_features
+    stddev = (2. / total_in_size)**.5
+    w_c = tf.Variable(tf.random_normal(
+      [kernel, kernel, in_features, output],
+      stddev=stddev,
+    ))
     conv = tf.nn.conv2d(in_tensor, w_c, strides=[1, 1, 1, 1], padding='SAME')
-    conv_w_bias = tf.add(conv, b_c)
+    bn_conv = tf.layers.batch_normalization(conv, training=training_mode)
     return activation_functions[activation](conv_w_bias)
 
 class ModelV2(ModelBase):
   def create_model(self):
+    features = assignments.get('features', 256)
     in_tensor = x
-    for i in range(assignments.get('n_conv', 2)):
+    in_tensor = create_conv_block(
+      in_tensor=in_tensor,
+      kernel=assignments.get('input_kernel', 7)
+      output=features,
+      activation='relu',
+    )
+    in_tensor = tf.layers.batch_normalization(in_tensor, training=training_mode)
+    for i in range(assignments.get('num_conv', 2)):
       in_tensor = create_conv_block(
-          in_tensor=in_tensor,
-          kernel=assignments.get('conv_kernel'.format(i), 5),
-          output=assignments.get('conv_output'.format(i), 64),
-          activation=assignments.get('conv_act'.format(i), 'relu'),
-      )
-      in_tensor = tf.layers.batch_normalization(in_tensor, training=training_mode)
+        in_tensor=in_tensor,
+        kernel=assignments.get('conv_kernel', 5),
+        output=features,
+        activation='relu',
+      ) + in_tensor
 
     return create_conv_block(
-        in_tensor=in_tensor,
-        kernel=assignments.get('output_kernel', 5),
-        output=1,
-        activation=assignments.get('output_act', 'sigmoid'),
+      in_tensor=in_tensor,
+      kernel=assignments.get('output_kernel', 32),
+      output=1,
+      activation='sigmoid',
     )
 
 if __name__ == '__main__':
