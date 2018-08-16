5bc091eed2367e59a0c6f1ed2617ae77ad8dfe7b
Taylor Jackle Spriggs <taylor@sigopt.com>
2018-08-16 10:44:55 -0700

    more updates to v2 model

--- detectors/model_v2.py
+++ detectors/model_v2.py
@@ -3,50 +3,50 @@ import tensorflow as tf
 from constants import activation_functions, assignments, x, y, training_mode
 from model_base import ModelBase
 
-def create_conv_block(in_tensor, kernel, output, activation):
+def conv2d(in_tensor, kernel, output):
     in_features = in_tensor.get_shape()[3].value
-    total_in_size = kernel**2 * in_features
-    stddev = (2. / total_in_size)**.5
-    w_c = tf.Variable(tf.random_normal(
-      [kernel, kernel, in_features, output],
-      stddev=stddev,
-    ))
-    conv = tf.nn.conv2d(in_tensor, w_c, strides=[1, 1, 1, 1], padding='SAME')
-    bn_conv = tf.layers.batch_normalization(conv, training=training_mode)
-    return activation_functions[activation](bn_conv)
+    return tf.nn.conv2d(
+      inputs=in_tensor,
+      filter=[kernel, kernel, in_features, output],
+      strides=[1, 1, 1, 1],
+      padding='SAME',
+      use_bias=False,
+      kernel_initializer=tf.variance_scaling_initializer(),
+    )
+
+def bn(in_tensor):
+  return tf.layers.batch_normalization(in_tensor, training=training_mode)
 
 class ModelV2(ModelBase):
   def create_model(self):
-    features = assignments.get('features', 256)
+    relu = activation_functions['relu']
+    sigmoid = activation_functions['sigmoid']
     in_tensor = x
-    in_tensor = create_conv_block(
+    in_tensor = relu(bn(conv2d(
       in_tensor=in_tensor,
       kernel=assignments.get('input_kernel', 7),
       output=features,
-      activation='relu',
-    )
+    )))
     for i in range(assignments.get('num_conv', 2)):
-      ident = in_tensor
-      in_tensor = create_conv_block(
+      block_features = assignments.get('block_features', 256)
+      shortcut = bn(conv2d(in_tensor, 1, block_features))
+      in_tensor = relu(bn(conv2d(
         in_tensor=in_tensor,
         kernel=assignments.get('conv1_kernel', 5),
-        output=features,
-        activation='relu',
-      )
-      in_tensor = create_conv_block(
+        output=assignments.get('conv1_features', 256),
+      )))
+      in_tensor = bn(conv2d(
         in_tensor=in_tensor,
         kernel=assignments.get('conv2_kernel', 5),
-        output=features,
-        activation='relu',
-      )
-      in_tensor = in_tensor + ident
+        output=block_features,
+      ))
+      in_tensor = in_tensor + shortcut
 
-    return create_conv_block(
+    return sigmoid(bn(conv2d(
       in_tensor=in_tensor,
       kernel=assignments.get('output_kernel', 32),
       output=1,
-      activation='sigmoid',
-    )
+    )))
 
 if __name__ == '__main__':
   from evaluator import train_and_evaluate
